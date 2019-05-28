ff0365ac1be9764598cf903cd0b8aff61c970c25
Paul Masri-Stone <paulmasri@gmail.com>
2019-05-28 22:54:51 +0100

    Player has thrust (space)

--- player-with-rotate-and-thrust/components/Player.gd
+++ player-with-rotate-and-thrust/components/Player.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
 
 var screen_size
+var thrust = Vector2(0, -250)
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	position.x = screen_size.x / 2
 	position.y = screen_size.y / 2
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _integrate_forces(state):
+	if (Input.is_action_pressed("ui_accept")):
+		applied_force = thrust.rotated(rotation)
+	else:
+		applied_force = Vector2()
