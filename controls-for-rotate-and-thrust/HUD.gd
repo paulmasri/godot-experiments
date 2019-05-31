bd2aba423d3b9bab14f9f69436a36313be0814a4
Paul Masri-Stone <paulmasri@gmail.com>
2019-05-31 21:59:29 +0100

    Thrust & torque are set to max values while space/left/right keys are being pressed

--- controls-for-rotate-and-thrust/HUD.gd
+++ controls-for-rotate-and-thrust/HUD.gd
@@ -13,6 +13,26 @@ func _ready():
 #	pass
 
 
+func _input(event):
+	if event is InputEventKey:
+		match(event.scancode):
+			KEY_SPACE:
+				if event.pressed:
+					set_thrust(MAX_THRUST)
+				else:
+					set_thrust(0)
+			KEY_LEFT:
+				if event.pressed:
+					set_torque(-MAX_TORQUE)
+				else:
+					set_torque(0)
+			KEY_RIGHT:
+				if event.pressed:
+					set_torque(MAX_TORQUE)
+				else:
+					set_torque(0)
+
+
 func set_thrust(thrust):
 	$"Thrust slider".value = thrust
 
