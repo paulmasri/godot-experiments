75eb26ea18128b3e975b77e2203460d30a71ff06
Paul Masri-Stone <paulmasri@gmail.com>
2019-06-20 11:51:46 +0100

    Hub has passthru signal level_selected for LevelSelectButtons

--- switch-between-levels-and-hub/Hub.gd
+++ switch-between-levels-and-hub/Hub.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal level_selected(scene)
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_LevelSelectButton1_level_selected(scene):
+	emit_signal("level_selected", scene)
+
+
+func _on_LevelSelectButton2_level_selected(scene):
+	emit_signal("level_selected", scene)
